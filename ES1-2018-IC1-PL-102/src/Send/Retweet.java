75eb16ab7ec18c5d990ebea4f45a68934b05864b
barfa-iscteiul <barfa@iscte-iul.pt>
2018-11-19 18:33:40 +0000

    T

--- ES1-2018-IC1-PL-102/src/Send/Retweet.java
+++ ES1-2018-IC1-PL-102/src/Send/Retweet.java
@@ -1,3 +1,17 @@
 package Send;
 
+import java.util.List;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
+public class Retweet{
+	
+	public static void retweet(List<Status> retweet) {
+		
+		
+		
+	}
+	
+}
