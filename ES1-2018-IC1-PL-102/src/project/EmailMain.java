a53faad0255a71391198cdca4c10363cec83e021
faons-iscteiul <faons@iscte-iul.pt>
2018-11-23 05:09:11 +0000

    Email terminado! Criação da classe Mensagem

--- ES1-2018-IC1-PL-102/src/project/EmailMain.java
+++ ES1-2018-IC1-PL-102/src/project/EmailMain.java
@@ -1,7 +1,5 @@
 package project;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -66,56 +64,61 @@ public class EmailMain {
 						BodyPart bodyPart = mimeMultipart.getBodyPart(j);
 						if (bodyPart.isMimeType("text/plain")){
 							result = result + "\n" + bodyPart.getContent();
-							info.add(message.getSubject() + " " + message.getFrom()[0] + " " + result);
-							break;  //aparecem mensagens duplicadas sem o break
-						} else if (bodyPart.isMimeType("text/html")){
-							String html = (String) bodyPart.getContent();
-							result = result + "\n" + Jsoup.parse(html).text();
-							info.add(message.getSubject() + " " + message.getFrom()[0] + " " + result);
+							if(result.contains(EmailFrame.textField.getText()) || message.getFrom().toString().contains(EmailFrame.textField.getText())
+									|| message.getSubject().contains(EmailFrame.textField.getText())) {
+								info.add(message.getSubject() + " " + message.getFrom()[0] + " " + result);
+								//info.add(message.getSubject() + " " + message.getFrom()[0] + " " + result);
+
+								break;  //aparecem mensagens duplicadas sem o break
+							} else if (bodyPart.isMimeType("text/html")){
+								String html = (String) bodyPart.getContent();
+								result = result + "\n" + Jsoup.parse(html).text();
+								info.add(message.getSubject() + " " + message.getFrom()[0] + " " + result);
+
+							}
+							//System.out.println("---------------------------------");
+							//System.out.println("Email Number " + (i + 1));
+							//System.out.println("Subject: " + message.getSubject());
+							//System.out.println("From: " + message.getFrom()[0]);
+							//System.out.println("Text: " + message.getContent().toString());
+							//info.add(message.getSubject() + " " + message.getFrom()[0]);
+							//info.add(message.getSentDate().toString());
+							//info.add(message.getContent().toString());
+							//System.out.println(info + "LISTA AQUI");
 
 						}
-						//System.out.println("---------------------------------");
-						//System.out.println("Email Number " + (i + 1));
-						//System.out.println("Subject: " + message.getSubject());
-						//System.out.println("From: " + message.getFrom()[0]);
-						//System.out.println("Text: " + message.getContent().toString());
-						//info.add(message.getSubject() + " " + message.getFrom()[0]);
-						//info.add(message.getSentDate().toString());
-						//info.add(message.getContent().toString());
-						//System.out.println(info + "LISTA AQUI");
-
 					}
 
 				}
 			}
 
 
-					//fechar tudo, store e objetos
-					EmailFrame.info2JList();
-					emailFolder.close(false);
-					store.close();
+			//fechar tudo, store e objetos
+			EmailFrame.info2JList();
+			emailFolder.close(false);
+			store.close();
 
-				} catch (NoSuchProviderException e) {
-					e.printStackTrace();
-				} catch (MessagingException e) {
-					e.printStackTrace();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-
-
-
-
-
-			public static void authenticate() {
-
-				String host = "outlook.office365.com";// change accordingly
-				String mailStoreType = "pop3";
-				String username = "faons@iscte-iul.pt";// change accordingly
-				String password = "Kironaxe32!!";// change accordingly
-				getMail(host, mailStoreType, username, password);
-			}
-
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
+
+
+
+
+
+	public static void authenticate() {
+
+		String host = "outlook.office365.com";// change accordingly
+		String mailStoreType = "pop3";
+		String username = "faons@iscte-iul.pt";// change accordingly
+		String password = "Kironaxe32!!";// change accordingly
+		getMail(host, mailStoreType, username, password);
+	}
+
+}
 
